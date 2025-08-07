712088386be42e4b668379d6c6dfde785af2c905
raabiyaal <rishaq@uchicago.edu>
2025-08-07 16:32:31 -0500

    Update4

--- app.py
+++ app.py
@@ -49,11 +49,10 @@ for label, y_val in lines.items():
         hoverinfo='skip'
     ))
 
-fig.update_layout(
-    title=dict(
-        text=None,
-        x=0.5,
-        xanchor='center'
+title=dict(
+    text='Estimates of the Credit Curve Spread: 75% – 25% LTVs<br>Mortgage Loans for the Years 1996 through 1Q 2025',
+    x=0.5,
+    xanchor='center'
     ),
     yaxis=dict(
         title="Estimated Annual Interest Expense (k<sub>d</sub>)",
